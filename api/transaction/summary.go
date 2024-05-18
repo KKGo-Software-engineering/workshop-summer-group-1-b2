e53f4005ed2b55cdebf74876b0bfa1fadb31de09
varissara wongprasit <varissara@techupth.com>
2024-05-18 16:20:31 +0700

    phone| feat: add get summary expense func

--- api/transaction/summary.go
+++ api/transaction/summary.go
@@ -17,3 +17,50 @@ type Balance struct {
 	TotalAmountSpent  float64 `json:"total_amount_spent"`
 	TotalAmountSaved  float64 `json:"total_amount_saved"`
 }
+
+func getTransection() []Transaction {
+	mockTransaction := []Transaction{
+		{
+			Id:              1,
+			Date:            "2024-04-30T09:00:00.000Z",
+			Amount:          1000,
+			Catergory:       "Food",
+			TransectionType: "expense",
+			Note:            "Lunch",
+			ImageUrl:        "https://example.com/image1.jpg",
+			SpenderId:       1,
+		},
+		{
+			Id:              2,
+			Date:            "2024-04-29T19:00:00.000Z",
+			Amount:          2000,
+			Catergory:       "Transport",
+			TransectionType: "income",
+			Note:            "Salary",
+			ImageUrl:        "https://example.com/image2.jpg",
+			SpenderId:       1,
+		},
+	}
+
+	return mockTransaction
+}
+
+func GetSummaryExpenses() SummaryExpenses {
+	transactions := getTransection()
+	totalAmountSpent := 0.0
+	totalNumberSpent := 0
+
+	for _, transaction := range transactions {
+		if transaction.TransectionType == "expense" {
+			totalAmountSpent += transaction.Amount
+			totalNumberSpent++
+		}
+	}
+
+	return SummaryExpenses{
+		TotalAmountSpent:     totalAmountSpent,
+		AvgAmountSpentPerDay: totalAmountSpent / float64(totalNumberSpent),
+		TotalNumberSpent:     totalNumberSpent,
+	}
+
+}
